ba3320e57c7e5be09cc1d1f3c1d43ce5ae06f4cc
daniel caamal <danielcaamal97@gmail.com>
2024-06-18 18:35:43 -0600

    FTR: Update

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, ParseIntPipe } from '@nestjs/common';
-import { ProductsService } from './products.service';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
-import { PaginationDto } from 'src/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
+
+import { PaginationDto } from 'src/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto, UpdateProductDto } from './dto';
 
 @Controller('products')
 export class ProductsController {
