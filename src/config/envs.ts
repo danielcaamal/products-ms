2e3f9ae01a8d4330d8ba042e932623c1c087e991
daniel caamal <danielcaamal97@gmail.com>
2024-06-19 18:36:30 -0600

    FTR: Nats server

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,16 +4,21 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   DATABASE_URL: string;
+  NATS_SERVERS: string[];
 }
 
 const envVarsSchema = joi
   .object({
     PORT: joi.number().default(3000),
     DATABASE_URL: joi.string().required(),
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
 
-const { error, value } = envVarsSchema.validate(process.env);
+const { error, value } = envVarsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
@@ -24,4 +29,5 @@ const config: EnvVars = value;
 export const envs = {
   PORT: config.PORT,
   DATABASE_URL: config.DATABASE_URL,
+  NATS_SERVERS: config.NATS_SERVERS,
 };
